458c4b3abc2cfdbef8eaa028540dbe9277e8ab56
RyanEAA <aparicioryan2003@gmail.com>
2025-02-24 20:57:49 -0600

    added workspace

--- MediapipeLandmarksTest/fruit_utils.py
+++ MediapipeLandmarksTest/fruit_utils.py
@@ -6,3 +6,4 @@ def get_random_fruit() -> str:
     fruits = ["Apple.png", "Banana.png", "BellPepper-orange_Half.png", "BellPepper-orange.png", "Jalepeño_Half.png", "Jalepeño.png", "Orange.png", "Tomato_Half.png", "Tomato.png"]
     return "/Users/ryan/Downloads/PFIcons/"+random.choice(fruits)
 
+# def spawn_fruits()
